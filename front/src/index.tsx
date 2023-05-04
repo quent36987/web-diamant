9971d06898b7591fa07eb59f7509044bbb8bedef
Quentin Goujon <quentin.goujon@epita.fr>
2023-05-04 19:02:25 +0200

    height

--- front/src/index.tsx
+++ front/src/index.tsx
@@ -4,16 +4,15 @@ import React from 'react';
 import ReportWebVitals from './reportWebVitals';
 import { createRoot } from 'react-dom/client';
 import { Context } from './componant/Context';
-import { ToastProvider } from './componant/toast';
 const container = document.getElementById('root');
 const root = createRoot(container); // createRoot(container!) if you use TypeScript
 
 root.render(
 
         <Context>
-            <ToastProvider>
+
         <Application />
-            </ToastProvider>
+
             </Context>
 
 
