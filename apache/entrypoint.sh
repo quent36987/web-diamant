868247fe5c4bde83b3b3708980c7926b6950275b
Quentin Goujon <quentin.goujon@epita.fr>
2023-10-27 23:10:33 +0200

    [FIX] change https to http to prepare for the serveur

--- apache/entrypoint.sh
+++ apache/entrypoint.sh
@@ -4,11 +4,11 @@
 DOMAIN="${DOMAIN}"
 
 # Get the certificate or renew it if needed
-certbot certonly --standalone --agree-tos --non-interactive --email quentin.goujon@epita.fr -d $DOMAIN --pre-hook "apachectl stop" --post-hook "apachectl start" --keep
+#certbot certonly --standalone --agree-tos --non-interactive --email quentin.goujon@epita.fr -d $DOMAIN --pre-hook "apachectl stop" --post-hook "apachectl start" --keep
 
 # Copy the certificates and keys
-cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem /usr/local/apache2/conf/server.crt
-cp /etc/letsencrypt/live/$DOMAIN/privkey.pem /usr/local/apache2/conf/server.key
+#cp /etc/letsencrypt/live/$DOMAIN/fullchain.pem /usr/local/apache2/conf/server.crt
+#cp /etc/letsencrypt/live/$DOMAIN/privkey.pem /usr/local/apache2/conf/server.key
 
 # Start Apache in the foreground
 httpd -D FOREGROUND
